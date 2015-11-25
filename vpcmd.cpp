b7bd8f81b2f41192211b36a2f9ab728b218f6025
Chris Anderson <Rails2240@gmail.com>
2015-11-25 15:22:23 -0500

    This is my virtual pet sim 0.1
    The following source is in C++

---
+++ vpcmd.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+
+{
+
+int B;
+
+//if  (2 == B ) {  --retunrn to start code
+	
+	
+
+
+
+int A;
+
+
+	cout << "Hi! I am your new virtual Dog!! What would you like to do? \n";
+
+		cout << "Feed, press 1 then enter. \n";
+		cout << "Do a trick, press 2 then enter. \n";
+
+cin >> A;
+
+
+if ( 1 == A  ) {
+
+
+cout << "*nums food happly* \n";
+
+
+//cin.get();
+
+}
+
+else if ( 2 == A) {
+
+cout << "*rolls over* \n";
+
+
+
+
+}
+
+else {
+
+
+}
+
+
+
+
+//} --extra loop stuff
+
+
+
+
+
+
+ system("pause");
+
+
+
+
+
+}
